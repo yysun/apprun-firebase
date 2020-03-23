f17390e98ee020667d7da566dd4a8e4fd9db9e52
Yiyi Sun <yiyisun@gmail.com>
2020-03-22 21:27:55 -0400

    add cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,27 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.updateTodo = functions.firestore.document('requests/{Id}')
+  .onWrite((change, context) => {
+    const { uid, event, data } = change.after.data();
+
+    const db = admin.firestore();
+    const todos = db.collection('/users/' + uid + '/todos');
+
+    switch (event) {
+      case '@create-todo':
+        todos.add(data);
+        break;
+      case '@update-todo':
+        todos.doc(data.id).update(data);
+        break;
+      case '@delete-todo':
+        todos.doc(data.id).delete();
+        break;
+      case '@delete-all-todo':
+        db.collection('/users').doc(uid).delete();
+      default: return;
+    }
+});
--- www/src/firebase.ts
+++ www/src/firebase.ts
@@ -4,12 +4,12 @@ declare var firebase;
 
 function init(uid) {
   const db = firebase.firestore();
-    db.collection(`users/${uid}/todos`)
-      .onSnapshot(s => app.run('@show-all-todos', s));
-    app.on('//:', (event, data) => {
-      db.collection(`events`).add({uid, event, data})
-    });
-  }
+  db.collection(`users/${uid}/todos`)
+    .onSnapshot(s => app.run('@show-all-todos', s));
+  app.on('//:', (event, data) => {
+    db.collection(`events`).add({ uid, event, data })
+  });
+}
 
 export default function () {
   firebase
