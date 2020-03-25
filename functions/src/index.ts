ba6e33bb8d58efbe55a630a2befa5192e259406e
Yiyi Sun <yiyisun@gmail.com>
2020-03-25 14:34:26 -0400

    fix functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,25 +3,30 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-exports.updateTodo = functions.firestore.document('requests/{Id}')
+exports.updateTodo = functions.firestore.document('events/{Id}')
   .onWrite((change, context) => {
-    const { uid, event, data } = change.after.data();
+    const { uid, event, data } = change.after.data() as any;
+
+    console.log('===>', uid, event, data);
 
     const db = admin.firestore();
     const todos = db.collection('/users/' + uid + '/todos');
 
     switch (event) {
       case '@create-todo':
-        todos.add(data);
-        break;
+        return todos.add(data);
       case '@update-todo':
-        todos.doc(data.id).update(data);
-        break;
+        return todos.doc(data.id).update(data);
       case '@delete-todo':
-        todos.doc(data.id).delete();
-        break;
+        return todos.doc(data.id).delete();
       case '@delete-all-todo':
-        db.collection('/users').doc(uid).delete();
+        return todos.get().then(function (querySnapshot) {
+          const batch = db.batch();
+          querySnapshot.forEach(function (doc) {
+            batch.delete(doc.ref);
+          });
+          return batch.commit();
+        });
       default: return;
     }
 });
